db001414ba2aa37168014883fd201d2e0e6e7b9e
maciektr <mtratnowiecki@gmail.com>
2022-04-02 19:52:39 +0200

    Fix port

--- backend/app.ts
+++ backend/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from "express";
 import logger from "morgan";
 
 const app: Application = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // Body parsing Middleware
 app.use(logger("dev"));
