cf96346dae71a31789c75e822fa60f7343bbc84c
mkozieja <mkozieja@qualtrics.com>
2022-04-02 19:06:30 +0200

    working with react18

---
+++ frontend/src/step/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// header + description with collapse + additional components
+
+type StepProps = { title: string, description: string }
+
+const Step = ({ title, description }: StepProps) => {
+
+
+    return <div>
+        <h1>{title}</h1>
+    </div>
+}
+
+export default Step
